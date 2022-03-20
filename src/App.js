24d5595480a099c7de1ff83ce6f4afddd2c084dd
Samip <callmesamip@gmail.com>
2022-03-21 09:55:02 +1100

    Working on useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useEffect,useState } from 'react';
 import './App.css';
+import { ContactCard } from './components/ContactCard';
 import { ContactList } from './components/ContactList';
 import { SearchFilter } from './components/SearchFilter';
 import { Title } from './components/Title';
+const apiUrl = 'https://randomuser.me/api/?results=10'
 function App() {
+  const [contacts,setContacts]=useState([]); 
+  useEffect(()=>{
+    fetch(apiUrl)
+    .then()
+  },[])
+  console.log("Rendering completed");
   return (
     <div className="wrapper">
       <div className="container">
@@ -14,6 +23,7 @@ function App() {
           <div className="col">55 user Found</div>
           </div>
           <ContactList></ContactList>
+          <ContactCard></ContactCard>
         </div>
     </div>
   );
