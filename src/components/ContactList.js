41a9b1b42ad34430e1848a9b24767edf1a1d9064
Samip <callmesamip@gmail.com>
2022-03-21 10:47:23 +1100

    ContactList now passes props to card

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Card } from './Card';
 
-export const ContactList = () => {
+export const ContactList = ({contact}) => {
   return (
     <div className="row">
-      <div className="col">
-      <div className="spinner-border text-primary" role="status">
-    <span className="visually-hidden">Loading...</span>
-      </div>
-        </div>
+     
+        {!contact.length ? (
+          <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+          </div>
+        ): contact.map((usr,i)=><Card key={i} user={usr}/>)}
       
+       
       </div>
   )
 }
