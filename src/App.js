62709a86c76a28233c5ae0a9f5615e502f0a2701
Samip <callmesamip@gmail.com>
2022-03-21 09:00:38 +1100

    Started integrating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import { SearchFilter } from './components/SearchFilter';
+import { Title } from './components/Title';
 function App() {
   return (
     <div className="wrapper">
       <div className="container">
-        
+        <Title/>
+        <SearchFilter></SearchFilter>
         </div>
     </div>
   );
