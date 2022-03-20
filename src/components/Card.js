7e523e3890768085b457588343a5d2da549664b5
Samip <callmesamip@gmail.com>
2022-03-21 10:55:14 +1100

    Working on displaying the user detail

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,15 @@ export const Card = ({user}) => {
   return (
     <div className="col-md-6 col-lg-3 py-2"> 
     <div className="card">
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src={user.picture.large} className="card-img-top" alt="..."/>
   <div className="card-body">
-  <h5 className="card-title">Card title</h5>
-  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <h5 className="card-title">{user.name.title} {user.name.first} {user.name.last}</h5>
+  <p className="card-text">
+    <ul className='list-unstyled'>
+      <li>Phone : {user.cell} </li>
+      <li>Email: {user.email}</li>
+      <li>Country : {user.location.country} </li>
+      </ul></p>
   <a href="#" className="btn btn-primary">Go somewhere</a>
 </div>
 </div>
